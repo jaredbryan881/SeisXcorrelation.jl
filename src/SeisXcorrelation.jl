cd7c8cfae4d84421542d56cd8905f0af93e97216
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-06 08:30:22 -0400

    Added station pair generation and sorting station pairs by correlation type. Minor documentation fixes.

--- src/SeisXcorrelation.jl
+++ src/SeisXcorrelation.jl
@@ -3,7 +3,7 @@ module SeisXcorrelation
 
 using SeisIO, Noise, Printf, Dates, FFTW, JLD2, MPI
 
-export seisxcorrelation
+export seisxcorrelation, sort_pairs
 
 
 """
@@ -29,9 +29,18 @@ function seisxcorrelation(maxtimelag::Real, finame::String, foname::String; IsAl
     size = MPI.Comm_size(comm)
     rank = MPI.Comm_rank(comm)
 
-    #Read validation data from ["../EXAMPLE/codevalidation"]
+    # read data from JLD2
+    data = jldopen(finame)
 
-    #Make Station pairs (after validating CC part, read from actual dataset and use statino list in SeisData structure)
+    # generate station pairs
+    station_pairs = generate_pairs(data["info/stationlist"])
+
+    # sort station pairs into autocorr, xcorr, and xchancorr
+    sorted_pairs = sort_pairs(station_pairs)
+
+    # if corr method == c2 or c3
+        # perform additional pairing on only xcorr
+
     #Take into account IsAllComponents == true or not
 
     mpiitrcount = 0
@@ -47,11 +56,12 @@ function seisxcorrelation(maxtimelag::Real, finame::String, foname::String; IsAl
 
         if rank == processID-1
 
-            #roop each station pair
+            #group each station pair
 
-            for j = 1:length("""stationpairlist`""")
+            for j = 1:length(stationPairs)
 
                 # compute crosscorrelation using Noise.jl
+
 
                 # store in SeisChannel structure
                 # time, fs, misc["stationpair"], misc["cc1"]
@@ -89,12 +99,57 @@ function seisxcorrelation(maxtimelag::Real, finame::String, foname::String; IsAl
         end
     end
 
-    if rank == 0 println("Crosscorrelation and Saving data is successfully done.\njob ended at "*string(now())) end
+    if rank == 0 println("Cross-correlation and data writing completed successfully.\nJob ended at "*string(now())) end
 
     MPI.Finalize()
 
 
     return 0
+end
+
+end
+
+
+"""
+
+    sort_pairs(pairs::AbstractArray)
+
+Sort station pairs into their correlation category (auto-correlation, cross-correlation, cross-channel-correlation)
+
+# Arguments
+- `pairs::AbstractArray,`    : Array of station pairs, e.g. output of generate_pairs(file["info/stationlist"])
+
+# Output
+- `sorted_pairs::Dict{String, Array{String, N}}`    : dictionary containing station pairs grouped by correlation type
+
+"""
+function sort_pairs(pairs::AbstractArray)
+    # dict containing each correlation category
+    # preallocation of dict size assumes all stations have the same number of channels, which may not be true.
+    sorted_pairs = Dict{String, Array{String}}("acorr"     => ["", ""],
+                                               "xcorr"     => ["", ""],
+                                               "xchancorr" => ["", ""])
+
+    # fill dictionary based on detected correlation type
+    for stnPair in pairs
+        # same station, same channel
+        if stnPair[1] == stnPair[2]
+            sorted_pairs["acorr"] = hcat(sorted_pairs["acorr  "], stnPair)
+        # same station, different channel
+        elseif (stnPair[1][end-3:end] != stnPair[2][end-3:end]) && (stnPair[1][1:end-3] == stnPair[2][1:end-3]
+            sorted_pairs["xchancorr"] = hcat(sorted_pairs["xchancorr"], stnPair)
+        # different station
+        else
+            sorted_pairs["xcorr"] = hcat(sorted_pairs["xcorr"], stnPair)
+        end
+    end
+
+    # Remove ["", ""] used to initialized the dict
+    sorted_pairs["acorr"]     = sorted_pairs["acorr"][:, 2:end]
+    sorted_pairs["xcorr"]     = sorted_pairs["xcorr"][:, 2:end]
+    sorted_pairs["xchancorr"] = sorted_pairs["xchancorr"][:, 2:end]
+
+    return sorted_pairs
 end
 
 end
