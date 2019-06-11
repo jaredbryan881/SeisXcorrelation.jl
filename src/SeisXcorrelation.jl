9cd9368325042e0248469ba43176c9bb0205b54a
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-11 11:38:40 -0400

    Changed corrtype determination.

--- src/SeisXcorrelation.jl
+++ src/SeisXcorrelation.jl
@@ -26,12 +26,11 @@ Compute cross-correlation function and save data in jld2 file with SeisData form
 - `foname.jld2`    : contains SeisData structure with a hierarchical structure (CC function, metadata)
 
 """
-@everywhere function seisxcorrelation(tstamp::String, finame::String, foname::String, corrtype::Array{String,1}, corrorder::Int, maxtimelag::Real, freqmin::Real, freqmax::Real, fs::Real, cc_len::Int, cc_step::Int, IsAllComponents::Bool=false)
+function seisxcorrelation(tstamp::String, finame::String, foname::String, corrtype::Array{String,1}, corrorder::Int, maxtimelag::Real, freqmin::Real, freqmax::Real, fs::Real, cc_len::Int, cc_step::Int, IsAllComponents::Bool=false)
     # iterate over station list
     for stn1 in stlist
         # read station SeisChannels into SeisData before FFT
         S1 = SeisData(data["$tstamp/$stn1"])
-
         # round start times to nearest millisecond to avoid split start times bug
         S1[1].t[1,2] = round(S1[1].t[1,2], sigdigits=13)
 
@@ -45,17 +44,18 @@ Compute cross-correlation function and save data in jld2 file with SeisData form
 
         # iterate over station list again
         for stn2 in stlist
+            @time begin
             println("$tstamp: corrrelating $stn1 with $stn2")
             # see if this is an auto-, cross-, or xchan-correlation
             ct = get_corrtype([stn1, stn2])
 
             # check if this is an autocorrelation
-            if ("acorr" in corrtype) && (ct=="acorr")
+            if (ct=="acorr") && ("acorr" in corrtype)
                 # set the stn2 FFT to the already computed FFT for stn1
                 FFT2 = FFT1
 
-            # check if this is a cross-channel correlation
-            elseif ("xchancorr" in corrtype) && (ct=="xchancorr")
+                # check if this is a cross-channel correlation
+            elseif (ct=="xchancorr") && ("xchancorr" in corrtype)
                 # read station SeisChannels into SeisData before FFT
                 S2 = SeisData(data["$tstamp/$stn2"])
                 # round start times to nearest millisecond to avoid split start times bug
@@ -68,12 +68,13 @@ Compute cross-correlation function and save data in jld2 file with SeisData form
                     exit()
                 end
 
-            # check if this is a cross-correlation
-            elseif ("xcorr" in corrtype) && (ct=="xcorr")
+                # check if this is a cross-correlation
+            elseif (ct=="xcorr") && ("xcorr" in corrtype)
                 # read station SeisChannels into SeisData before FFT
                 S2 = SeisData(data["$tstamp/$stn2"])
                 # round start times to nearest millisecond to avoid split start times bug
                 S2[1].t[1,2] = round(S2[1].t[1,2], sigdigits=13)
+
                 # check correlation order and compute the appropriate FFT using Noise.jl
                 if corrorder == 1
                     FFT2 = compute_fft(S2, freqmin, freqmax, fs, cc_step, cc_len)
@@ -93,6 +94,7 @@ Compute cross-correlation function and save data in jld2 file with SeisData form
             # save data after each cross correlation
             varname = "$tstamp/$stn1.$stn2"
             try save_CorrData2JLD2(foname, varname, xcorr) catch; println("$stn1 and $stn2 have no overlap at $tstamp.") end
+            end
         end
     end
 end
