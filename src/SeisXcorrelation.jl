f77d0decae1ae34f359f4d2dff8994406473f65f
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-11 15:40:42 -0400

    Added exception handling for missing data and for dimension mismatches during FFT.

--- src/SeisXcorrelation.jl
+++ src/SeisXcorrelation.jl
@@ -27,57 +27,82 @@ Compute cross-correlation function and save data in jld2 file with SeisData form
 
 """
 function seisxcorrelation(tstamp::String, finame::String, foname::String, corrtype::Array{String,1}, corrorder::Int, maxtimelag::Real, freqmin::Real, freqmax::Real, fs::Real, cc_len::Int, cc_step::Int, IsAllComponents::Bool=false)
+    stniter = 1 # counter to enforce computing only unique cross-correlations
     # iterate over station list
     for stn1 in stlist
         # read station SeisChannels into SeisData before FFT
         S1 = SeisData(data["$tstamp/$stn1"])
+        # do not attempt fft if data was not available
+        if S1[1].misc["dlerror"] == 1 println("$tstamp/$stn1 unavailable."); continue end
         # round start times to nearest millisecond to avoid split start times bug
         S1[1].t[1,2] = round(S1[1].t[1,2], sigdigits=13)
 
         # check correlation order and compute the appropriate FFT
         if corrorder == 1
-            FFT1 = compute_fft(S1, freqmin, freqmax, fs, cc_step, cc_len)
+            FFT1 = try
+                compute_fft(S1, freqmin, freqmax, fs, cc_step, cc_len)
+            catch y
+                if isa(y, DimensionMismatch)
+                    println("$tstamp: $stn1 has a dimension mismatch"); continue
+                end
+            end
         else
             println("Higher order correlation methods are not yet implemented\n")
             exit()
         end
 
         # iterate over station list again
-        for stn2 in stlist
-            @time begin
+        for stn2 in stlist[stniter:end]
             println("$tstamp: corrrelating $stn1 with $stn2")
+
             # see if this is an auto-, cross-, or xchan-correlation
             ct = get_corrtype([stn1, stn2])
 
-            # check if this is an autocorrelation
+            # autocorrelation
             if (ct=="acorr") && ("acorr" in corrtype)
                 # set the stn2 FFT to the already computed FFT for stn1
                 FFT2 = FFT1
 
-                # check if this is a cross-channel correlation
+            # cross-channel correlation
             elseif (ct=="xchancorr") && ("xchancorr" in corrtype)
                 # read station SeisChannels into SeisData before FFT
                 S2 = SeisData(data["$tstamp/$stn2"])
+                # do not attempt fft if data was not available
+                if S2[1].misc["dlerror"] == 1 println("$tstamp/$stn2 unavailable."); continue end
                 # round start times to nearest millisecond to avoid split start times bug
                 S2[1].t[1,2] = round(S2[1].t[1,2], sigdigits=13)
                 # check correlation order and compute the appropriate FFT using Noise.jl
                 if corrorder == 1
-                    FFT2 = compute_fft(S2, freqmin, freqmax, fs, cc_step, cc_len)
+                    FFT2 = try
+                        compute_fft(S2, freqmin, freqmax, fs, cc_step, cc_len)
+                    catch y
+                        if isa(y, DimensionMismatch)
+                            println("$tstamp: $stn2 has a dimension mismatch"); continue
+                        end
+                    end
                 else
                     println("Higher order correlation methods are not yet implemented\n")
                     exit()
                 end
 
-                # check if this is a cross-correlation
+            # cross-correlation
             elseif (ct=="xcorr") && ("xcorr" in corrtype)
                 # read station SeisChannels into SeisData before FFT
                 S2 = SeisData(data["$tstamp/$stn2"])
+                # do not attempt fft if data was not available
+                if S2[1].misc["dlerror"] == 1 println("$tstamp/$stn2 unavailable."); continue end
                 # round start times to nearest millisecond to avoid split start times bug
                 S2[1].t[1,2] = round(S2[1].t[1,2], sigdigits=13)
 
                 # check correlation order and compute the appropriate FFT using Noise.jl
                 if corrorder == 1
-                    FFT2 = compute_fft(S2, freqmin, freqmax, fs, cc_step, cc_len)
+                    FFT2 = try
+                        compute_fft(S2, freqmin, freqmax, fs, cc_step, cc_len)
+                    catch y
+                        if isa(y, DimensionMismatch)
+                            println("$tstamp: $stn2 has a dimension mismatch"); continue
+                        end
+                    end
                 else corrorder == 2
                     println("Higher order correlation methods are not yet implemented\n")
                     exit()
@@ -94,7 +119,7 @@ function seisxcorrelation(tstamp::String, finame::String, foname::String, corrty
             # save data after each cross correlation
             varname = "$tstamp/$stn1.$stn2"
             try save_CorrData2JLD2(foname, varname, xcorr) catch; println("$stn1 and $stn2 have no overlap at $tstamp.") end
-            end
         end
+        stniter += 1
     end
 end
