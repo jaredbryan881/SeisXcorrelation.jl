55ec47b6fc864aa171576636dd9683e8bb41d601
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-17 16:42:57 -0400

    Added utility to compute the distance between two stations (relies on Geodesy.jl).

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,5 @@
+using Geodesy
+
 """
     save_CorrData2JLD2(foname::String, varname::String, CD::CorrData)
 
@@ -38,4 +40,17 @@ end
 """
 function rms(A::AbstractArray, B::AbstractArray)
     return sqrt(sum((B.-A).^2)/length(A))
+end
+
+"""
+    dist(loc1::GeoLoc, loc2::GeoLoc)
+
+    Compute the distance (m) between two points (lat, lon, elev)
+    This assumes the distance is not large, such that the curvature of the earth is negligible.
+"""
+function dist(loc1::GeoLoc, loc2::GeoLoc)
+    loc1_lla = LLA(loc1.lat, loc1.lon, loc1.el)
+    loc2_lla = LLA(loc2.lat, loc2.lon, loc2.el)
+
+    return distance(loc1_lla, loc2_lla)
 end
