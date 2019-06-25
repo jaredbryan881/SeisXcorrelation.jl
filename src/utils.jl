e1b56edf5145613aa4860107b95e2a3a66bf5b2e
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-25 09:16:42 -0400

    Moved io utilities to io.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,37 +1,5 @@
-using Geodesy, JLD2
-
-"""
-    save_CorrData2JLD2(foname::String, varname::String, CD::CorrData)
-
-    save CorrData structure to JLD2
-"""
-function save_CorrData2JLD2(foname::String, varname::String, CD::CorrData)
-    file = jldopen(foname, "a+")
-    file[varname] = CD
-    JLD2.close(file)
-end
-
-"""
-    save_Dict2JLD2(foname::String, varname::String, D::Dict)
-
-    save Dictionary to JLD2
-"""
-function save_Dict2JLD2(foname::String, varname::String, D::Dict)
-    file = jldopen(foname, "r+")
-    file[varname] = D
-    JLD2.close(file)
-end
-
-"""
-    save_Dict2JLD2(foname::String, varname::String, D::Dict)
-
-    save Dictionary to JLD2
-"""
-function save_Array2JLD2(foname::String, varname::String, A::AbstractArray)
-    file = jldopen(foname, "a+")
-    file[varname] = A
-    JLD2.close(file)
-end
+using Geodesy
+export rms, dist
 
 """
     rms(A::AbstractArray, B::AbstractArray)
