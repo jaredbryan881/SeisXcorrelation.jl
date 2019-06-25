7b240db252729708cf1da2794a533f04790d3c87
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-25 17:23:10 -0400

    Added universal copy utility, updated documentation.

--- src/utils.jl
+++ src/utils.jl
@@ -4,7 +4,7 @@ export rms, dist
 """
     rms(A::AbstractArray, B::AbstractArray)
 
-    Compute the root mean squared error between two arrays
+Compute the root mean squared error between two arrays
 """
 function rms(A::AbstractArray, B::AbstractArray)
     return sqrt(sum((B.-A).^2)/length(A))
@@ -13,8 +13,8 @@ end
 """
     dist(loc1::GeoLoc, loc2::GeoLoc)
 
-    Compute the distance (m) between two points (lat, lon, elev)
-    This assumes the distance is not large, such that the curvature of the earth is negligible.
+Compute the distance (m) between two points (lat, lon, elev)
+This assumes the distance is not large, such that the curvature of the earth is negligible.
 """
 function dist(loc1::GeoLoc, loc2::GeoLoc)
     loc1_lla = LLA(loc1.lat, loc1.lon, loc1.el)
@@ -22,3 +22,10 @@ function dist(loc1::GeoLoc, loc2::GeoLoc)
 
     return distance(loc1_lla, loc2_lla)
 end
+
+"""
+    Base.copy(x::T)
+
+Universal copy function used to copy composite types such as structs.
+"""
+Base.copy(x::T) where T = T([deepcopy(getfield(x, k)) for k ∈ fieldnames(T)]...)
