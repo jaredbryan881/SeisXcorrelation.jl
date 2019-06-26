095d364ff52cff313c47957d6a32048d259e0d71
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-06-26 12:42:04 -0400

    Modified SeisXcorrelation test script to directly call the tested function.

--- EXAMPLE/codevalidation/Xcorrelation/testSeisXcorrelation.jl
+++ EXAMPLE/codevalidation/Xcorrelation/testSeisXcorrelation.jl
@@ -1,59 +1,41 @@
 using SeisIO, FileIO, JLD2, Noise, FFTW, PlotlyJS, Dates
 
-# This is a simple test script for basic Noise.jl functionality. 
+include("../../../src/SeisXcorrelation.jl")
+include("../../../src/pairing.jl")
+
+# This is a simple test script for basic Noise.jl functionality.
 # This is the core process used in SeisXcorrelation.jl
-# Read data from JLD2 -> Package as SeisData -> FFT -> XCorr -> Save as JLD2 
+# Read data from JLD2 -> Package as SeisData -> FFT -> XCorr -> Save as JLD2
 
 # read time series data from JLD2
 finame="sinStretchTest.jld2"
-f=jldopen(finame)
-u1 = f["u1"]
-u2 = f["u2"]
 
-# fft params
-freqmin = 0.01
-freqmax = 9.9
-fs      = 20.0
-cc_len  = 58
-cc_step = 1
-# xcorr params
-maxlag  = 100.0
-N       = 1200
+InputDict = Dict( "finame"     => "./sinStretchTest.jld2",
+                  "foname"     => "testData.jld2",
+                  "freqmin"    => 0.01,
+                  "freqmax"    => 9.9,
+                  "fs"         => 20.0,
+                  "cc_len"     => 58,
+                  "cc_step"    => 1,
+                  "corrtype"   => ["xcorr", "acorr", "xchancorr"],
+                  "corrorder"  => 1,
+                  "maxtimelag" => 100.0,
+                  "allstack"   => true)
 
-# put time series data into SeisChannel objects
-# first SeisChannel
-Ch1 = SeisChannel()
-Ch1.x = u1
-Ch1.name = "u1"
-t1=Array{Int64, 2}(undef, 2, 2)
-t1[1,1]=1
-t1[1,2]=0 # arbitrary choice
-t1[2,1]=length(u1)
-t1[2,2]=0
-Ch1.t=t1
-Ch1.fs=fs
-# second SeisChannel
-Ch2 = SeisChannel()
-Ch2.x = u2
-Ch2.name = "u2"
-t2=Array{Int64, 2}(undef, 2, 2)
-t2[1,1]=1
-t2[1,2]=0 # arbitrary choice
-t2[2,1]=length(u2)
-t2[2,2]=0
-Ch2.t=t2
-Ch2.fs=fs
+data=jldopen(finame)
+stlist = data["info/stationlist"]
+tstamplist = data["info/DLtimestamplist"]
+station_pairs = generate_pairs(stlist)
+sorted_pairs = sort_pairs(station_pairs)
 
-# put SeisChannel objects into SeisData object
-SD1 = SeisData(Ch1)
-SD2 = SeisData(Ch2)
+# create output file and save station and pairing information in JLD2
+jldopen((InputDict["foname"]), "w") do file
+    file["info/timestamplist"]   = tstamplist;
+    file["info/stationlist"]     = stlist;
+    file["info/corrstationlist"] = sorted_pairs;
+    file["info/tserrors"]        = []
+end
 
-fft1 = compute_fft(SD1, freqmin, freqmax, fs, cc_step, cc_len)
-fft2 = compute_fft(SD2, freqmin, freqmax, fs, cc_step, cc_len)
-fft1.name = "fft1"
-fft2.name = "fft2"
-
-xcorr = compute_cc(fft2, fft1, maxlag)
-tRange = collect(-length(xcorr.corr)/2:length(xcorr.corr)/2 - 1)*(1/fs)
-
-@save "./xcorrValidation.jld2"
+# compute xcorr
+errors = seisxcorrelation(tstamplist[1], stlist, data, InputDict)
+close(data)
