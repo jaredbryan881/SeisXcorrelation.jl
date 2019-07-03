67c85960959b25a2e35c4f8a6661b6d8e611f10e
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-07-03 17:35:59 -0400

    Added read_JLD22Dict function equivalent for arrays of JLD2 files. Moved window dependence outside of function.

--- src/io.jl
+++ src/io.jl
@@ -2,11 +2,23 @@ using JLD2
 export read_JLD22Dict, save_CorrData2JLD2, save_Dict2JLD2, save_Array2JLD2
 
 """
-    read_JLD22Dict(inFile, times::Array{Int64,1})
+    read_JLD22Dict(inFile::Array{JLD2.JLDFile,1}, tstamplist::AbstractArray, times::Array{Int64,1})
 
     read data structures from JLD2 into a list of dictionaries
 """
-function read_JLD22Dict(inFile, tstamplist, times::Array{Int64,1})
+function read_JLD22Dict(inFile::Array{JLD2.JLDFile{JLD2.MmapIO},1}, tstamplist::AbstractArray)
+    # create a list of dictionaries containing input data for each time step
+    # this assumes inFile is in the same order as tstamplist
+    dsets = [Dict("$ts/$stn" => inFile[i]["$ts/$stn"] for stn in keys(inFile[i][ts])) for (i, ts) in enumerate(tstamplist)]
+    return dsets
+end
+
+"""
+    read_JLD22Dict(inFile, tstamplist::AbstractArray, times::Array{Int64,1})
+
+    read data structures from JLD2 into a list of dictionaries
+"""
+function read_JLD22Dict(inFile::JLD2.JLDFile, tstamplist::AbstractArray)
     # create a list of dictionaries containing input data for each time step
     dsets = [Dict("$ts/$stn" => inFile["$ts/$stn"] for stn in keys(inFile[ts])) for ts in tstamplist[times]]
     return dsets
