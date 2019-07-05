86fdedd81992ec9f329124e5c71cb7e872088157
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-07-05 10:42:59 -0400

    Updated seisxcorrelation test.

--- EXAMPLE/codevalidation/Xcorrelation/testSeisXcorrelation.jl
+++ EXAMPLE/codevalidation/Xcorrelation/testSeisXcorrelation.jl
@@ -1,4 +1,4 @@
-using SeisIO, FileIO, JLD2, SeisNoise, FFTW, PlotlyJS, Dates
+using SeisIO, JLD2, SeisNoise, FFTW, Dates
 
 include("../../../src/SeisXcorrelation.jl")
 include("../../../src/pairing.jl")
@@ -7,11 +7,9 @@ include("../../../src/pairing.jl")
 # This is the core process used in SeisXcorrelation.jl
 # Read data from JLD2 -> Package as SeisData -> FFT -> XCorr -> Save as JLD2
 
-# read time series data from JLD2
-finame="sinStretchTest.jld2"
-
 InputDict = Dict( "finame"     => "./sinStretchTest.jld2",
-                  "foname"     => "testData.jld2",
+                  "basefoname" => "testData",
+                  "timeunit"   => 60,
                   "freqmin"    => 0.01,
                   "freqmax"    => 9.9,
                   "fs"         => 20.0,
@@ -22,20 +20,21 @@ InputDict = Dict( "finame"     => "./sinStretchTest.jld2",
                   "maxtimelag" => 100.0,
                   "allstack"   => true)
 
-data=jldopen(finame)
+data=jldopen(InputDict["finame"])
 stlist = data["info/stationlist"]
 tstamplist = data["info/DLtimestamplist"]
 station_pairs = generate_pairs(stlist)
 sorted_pairs = sort_pairs(station_pairs)
 
 # create output file and save station and pairing information in JLD2
-jldopen((InputDict["foname"]), "w") do file
+jldopen((InputDict["basefoname"])*".jld2", "w") do file
     file["info/timestamplist"]   = tstamplist;
     file["info/stationlist"]     = stlist;
     file["info/corrstationlist"] = sorted_pairs;
     file["info/tserrors"]        = []
 end
 
+dsets = read_JLD22Dict(data, tstamplist)
 # compute xcorr
-errors = seisxcorrelation(tstamplist[1], stlist, data, InputDict)
+errors = seisxcorrelation(dsets[1], tstamplist[1], InputDict)
 close(data)
