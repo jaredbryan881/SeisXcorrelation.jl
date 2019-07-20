6fa4b3ec01b45ee9172984ac51b0116720be0955
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-07-20 12:40:12 -0400

    Added SNR convergence function, and fixed RMS convergence function.

--- src/reference.jl
+++ src/reference.jl
@@ -3,75 +3,70 @@ include("utils.jl")
 include("io.jl")
 
 """
-
-    compute_reference_xcorr(finame::String, foname::String)
+compute_reference_xcorr(finame::String, foname::String)
 
 Compute cross-correlation function and save data in jld2 file with SeisData format.
 
 # Arguments
-- `finame::String,`    : Input file name e.g. "./inputData/BPnetwork.jld2"
+- `basefiname::String,`    : Input base file name e.g. "./inputData/BPnetwork"
 - `foname::String,`    : Output file name for fully stacked xcorrs e.g. "referenceXcorr.jld2"
-- `xcorrSize::Int,`    : Number of points in a cross-correlation window
 
 # Output
 - `foname.jld2`    : contains arrays of reference cross-correlations for each station pair
 
 """
-function compute_reference_xcorr(finame::String, foname::String, xcorrSize::Int)
-    f = jldopen(finame)
+function compute_reference_xcorr(basefiname::String, foname::String)
+    # input file holds metadata (stationlist, timestamplist, etc.)
+    f = jldopen(basefiname*".jld2")
+    tslist = f["info/timestamplist"]
+    close(f) # base xcorr file
 
-    corrstationlist = f["info/corrstationlist"] # names of station pairs
-    tslist = f["info/timestamplist"] # time stamps
-    reference_xcorrnames = []
+    # hold reference xcorrs in memory and write all at once
+    ref_dict = Dict()
 
-    # iterate over correlation type
-    for ct in keys(corrstationlist)
+    # iterate over timestamps
+    for tstamp in tslist
+        # read unstacked xcorrs for each time stamp
+        f_cur = jldopen(basefiname*".$tstamp.jld2")
+        grp = f_cur[tstamp] # xcorrs
+        println("stacking xcorrs at $tstamp")
+
         # iterate over station pairs
-        for stnum=1:size(corrstationlist[ct])[2]
-            # get name of station pair
-            stn1 = corrstationlist[ct][1, stnum]
-            stn2 = corrstationlist[ct][2, stnum]
-            varname = "$stn1" .* "." .* "$stn2"
-            println(varname)
+        for pair in keys(grp)
+            # load xcorr and reverse if necessary
+            xcorr = grp[pair]
 
-            # initialize array to hold stacked data
-            stackData = zeros(xcorrSize)
-            # num of summed xcorrs
-            numxcorr = 0
-            # compute full stack for this station pair
-            for tstamp in tslist
-                # do not attempt stacking if there was an error in cross-correlation
-                if "$tstamp/$stn1" in f["info/tserrors"] || "$tstamp/$stn2" in f["info/tserrors"]
-                    continue
-                end
+            # stack xcorrs over DL_time_unit (or over time length of CorrData object)
+            stack!(xcorr, allstack=true)
 
-                # read data and stack
-                data = try f["$tstamp/$varname"].corr catch; continue end
-                stackData += sum(data, dims=2)
-                numxcorr += size(data)[2]
+            # stack xcorrs if they have a key, assign key if not
+            if haskey(ref_dict, pair)
+                ref_dict[pair].corr .+= xcorr.corr
+            else
+                ref_dict[pair] = xcorr
             end
-            # compute average
-            stackData /= numxcorr
-
-            # save reference xcorr
-            save_Array2JLD2(foname, "$varname", stackData)
-            push!(reference_xcorrnames, varname)
         end
+        close(f_cur) # current xcorr file
     end
-    close(f)
-    save_Array2JLD2(foname, "info/reference_xcorrnames", reference_xcorrnames)
+
+    # write output file of reference xcorrs
+    f_ref = jldopen(foname, "a+")
+    for pair in keys(ref_dict)
+        f_ref[pair] = ref_dict[pair]
+    end
+    close(f_ref) # reference xcorr file
 end
 
 """
+convergence(finame::String, foname::String; metric::String="snr", skipoverlap::Bool=false)
 
-    convergence(finame::String, foname::String)
-
-Compute the RMS error between progressively longer stacks of cross-correlations and a reference cross-correlation
+Compute the RMS error or SNR for progressively longer stacks of cross-correlations and (for RMS) a reference cross-correlation
 
 # Arguments
 - `finame::String,`    : Input file name for unstacked cross-correlations e.g. "./inputData/BPnetwork.jld2"
 - `refname::String,`    : Input file name for reference cross-corrleations e.g. "./inputData/BPnetwork.jld2"
 - `foname::String,`    : Output file name for fully stacked xcorrs e.g. "referenceXcorr.jld2"
+- `metric::String`    : Method of tracking convergence: "snr" for peak signal-to-noise ratio or "rms" for root mean square misfit
 - `skipoverlap::Bool`    : Whether or not to skip overlap in cross-correlation windows. If cross-correlations were computed
                            with no overlap, set this to false to ensure no time is skipped.
 
@@ -79,43 +74,77 @@ Compute the RMS error between progressively longer stacks of cross-correlations 
 - `foname.jld2`    : contains arrays of RMS errors for progressively longer stacks of cross-correlations
 
 """
-function convergence(finame::String, refname::String, foname::String, skipoverlap::Bool=false)
-    f = jldopen(finame)
+function convergence(basefiname::String, refname::String, foname::String; metric::String="snr", skipoverlap::Bool=false)
+    # input file contains metadata (stationlist, timestamplist)
+    f = jldopen(basefiname*".jld2")
+    tslist = f["info/timestamplist"] # time stamps
+    close(f) # input file
+
+    # read reference xcorrs
     ref_f = jldopen(refname)
 
-    tslist = f["info/timestamplist"] # time stamps
-    stname = ref_f["info/reference_xcorrnames"] # station pairs with computed reference cross-correlations
+    # dictionary to keep track of convergence vectors for each station pair
+    conv = Dict()
+    # dictionary to keep track of stacked data
+    stackData = Dict{String, Array{Float64,2}}()
 
-    # iterate over station pairs
-    for stpair in stname
-        # reference xcorr
-        ref = ref_f[stpair]
-        # initialize running stack with size of the reference
-        stackData = zeros(size(ref))
-        # initialize rmse array
-        rms_conv = []
+    # iterate over timestamps
+    for tstamp in tslist
+        println("Processing $tstamp")
+        # read xcorrs for each time step
+        f_cur = jldopen(basefiname*".$tstamp.jld2")
 
-        iter=1
-        # iterate over timestamps
-        for tstamp in tslist
-            println("Processing $stpair/$tstamp")
-            data = try f["$tstamp/$stpair"].corr catch; continue end
+        # iterate over station pairs
+        for stnpair in keys(f_cur[tstamp])
+            # load unstacked cross-correlations
+            data = try f_cur["$tstamp/$stnpair"] catch; continue end
+
+            # load reference cross-correlation for the current station pair
+            ref = ref_f[stnpair].corr
+
             # iterate over windowed cross-correlations
-            for i=1:size(data)[2]
-                if skipoverlap
-                    # assume overlap is half of window length and skip overlapping windows
-                    i=2*i - 1
-                end
-                # keep running sum of cross-correlations
-                stackData += data[:, i]
+            for i=1:size(data.corr)[2]
+                if skipoverlap i=2*i-1 end # assumes cc_step is 1/2 cc_len
 
-                # compute RMS error of the average of the running sum with the reference
-                push!(rms_conv, rms(ref[:,1], stackData[:,1] ./ iter))
-                # keep track of the number of xcorrs in the running sum
-                iter += 1
+                # build input to convergence functions
+                if haskey(stackData, stnpair) && metric=="rms"
+                    # keep running sum of cross-correlations, stack if key exists, assign if not
+                    stackData[stnpair] .+= data.corr[:, i]
+                elseif haskey(stackData, stnpair) && metric=="snr"
+                    # create matrix of xcorrs
+                    stackData[stnpair] = hcat(stackData[stnpair], data.corr[:,i])
+                else
+                    # reshape into 2D array for snr input
+                    stackData[stnpair] = reshape(data.corr[:, i], length(data.corr[:,i]), 1)
+                end
+
+                # compute convergence data point and append to vector
+                if metric=="rms"
+                    # compute RMS error of the running sum with the reference
+                    if haskey(conv, stnpair)
+                        push!(conv[stnpair], rms(normalize(ref[:,1]), normalize(stackData[stnpair][:,1])))
+                    else
+                        conv[stnpair] = [rms(normalize(ref[:,1]), normalize(stackData[stnpair][:,1]))]
+                    end
+                elseif metric=="snr"
+                    # compute peak signal-to-noise ratio of increasingly long stacks
+                    psnr = maximum(snr(stackData[stnpair], data.fs))
+                    if haskey(conv, stnpair)
+                        push!(conv[stnpair], psnr)
+                    else
+                        conv[stnpair] = [psnr]
+                    end
+                end
             end
         end
-        # write RMS errors to foname
-        save_Array2JLD2(foname, stpair, rms_conv)
+        close(f_cur) # xcorrs for each time step
     end
+    close(ref_f) # reference xcorrs
+
+    # write convergence vectors to disk
+    f_conv = jldopen(foname, "a+")
+    for key in keys(conv)
+        f_conv[key] = conv[key]
+    end
+    close(f_conv) # convergence file
 end
