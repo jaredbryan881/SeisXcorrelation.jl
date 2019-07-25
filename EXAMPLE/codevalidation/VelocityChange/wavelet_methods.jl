1f04f67d8c29e359919b389258f045c601c9e1d5
Jared <jared@dhcp-140-247-105-249.fas.harvard.edu>
2019-07-25 10:28:27 -0400

    Fixed cwt implementation.

--- EXAMPLE/codevalidation/VelocityChange/wavelet_methods.jl
+++ EXAMPLE/codevalidation/VelocityChange/wavelet_methods.jl
@@ -49,38 +49,39 @@ end
 Wavelet coherence transform
 Modified from original by Congcong Yuan
 """
-function wct_modified(y1::Union{Array{Float64,1}, Array{Float32,1}}, y2::Union{Array{Float64,1}, Array{Float32,1}}, dt::Float64, dj::Float64, s0::Int64, J::Int64; wavelet::String="morlet", norm::Bool=true, sig::Bool=false, siglvl::Float64=0.95)
+function wct_modified(y1::Union{Array{Float64,1}, Array{Float32,1}}, y2::Union{Array{Float64,1}, Array{Float32,1}}, dt::Float64, dj::Float64, s0::Int64; J::Int64=109, wavelet::String="morlet", norm::Bool=true, sig::Bool=false, siglvl::Float64=0.95)
     # define wavelet
-    # check input params
-    if s0 == -1
-        s0 = 2 * dt / flambda
-    end
-    if J == -1
-        J = convert(Int64, round(log2(length(y1) * dt / s0) / dj))
+    if wavelet=="morlet"
+        wav = WT.Morlet()
     end
 
-    # make sure input signals are arrays
     # calculate std dev of both input signals (std(y1), std(y2))
-
+    std1 = std(y1)
+    std2 = std(y2)
+    # normalize
     if norm
-        y1 = (y1 - mean(y1)) / std1
-        y2 = (y2 - mean(y2)) / std2
+        y1 = (y1 .- mean(y1)) ./ std1
+        y2 = (y2 .- mean(y2)) ./ std2
     end
 
     # calculate the CWT of the time series, using identical parameters for both calculations
-    W1, sj, freq, coi, _, _ = cwt(y1, dt, dj, s0, J, wavelet)#############
-    W2, sj, freq, coi, _, _ = cwt(y2, dt, dj, s0, J, wavelet)
+    W1 = cwt(y1, wav, J1=J)
+    W2 = cwt(y2, wav, J1=J)
+
+    per, sj, coi = Transforms.caveats(y1, wav)
+    freq = 1 ./ per
 
     scales1 = ones(length(y1)) .* sj
     scales2 = ones(length(y2)) .* sj
 
     # smooth wavelet spectra before truncating
-    S1 = wavelet.smooth(abs.(W1).^2 ./ scales1, dt, dj, sj) ##########
-    S2 = wavelet.smooth(abs.(W2).^2 ./ scales1, dt, dj, sj)
+    S1 = smooth(abs.(W1).^2 ./ scales1, dt, dj, sj) ##########
+    S2 = smooth(abs.(W2).^2 ./ scales1, dt, dj, sj)
 
     # compute wavelet transform coherence
     W12 = W1 .* conj(W2)
-    scales = ones(length(y1)) .* sj
+    scales = ones(length(y1)) .* sj1
+
     S12 = wavelet.smooth(W12 ./ scales, dt, dj, sj) ############
     WCT = abs.(S12).^2 ./ (S1 .* S2)
     aWCT = angle.(W12)
@@ -105,7 +106,7 @@ end
 """
 Compute wavelet cross spectrum given an array of frequency band minima and maxima.
 """
-function wxs_freqbands(cur, ref, t, twindow, freqbands, dj, s0, J; wvn::String="morlet", unwrap::Bool=false, sig::Bool=false, siglvl::Float64=0.95)
+function wxs_freqbands(cur, ref, t, twindow, freqbands, dj, s0; J::S=NaN, wvn::String="morlet", unwrap::Bool=false, sig::Bool=false, siglvl::Float64=0.95) where S<:Real
     # define sample frequency
     dt = t[2] - t[1]
     fs = 1/dt
@@ -119,5 +120,5 @@ function wxs_freqbands(cur, ref, t, twindow, freqbands, dj, s0, J; wvn::String="
     cur = cur[t_ind]
     ref = ref[t_ind]
 
-    WXS, WXA, WCT, aWCT, coi, freq, sig = wct_modified(cur, ref, dt, dj, s0, J, wavelet=wvn, sig=sig, siglvl=siglvl)
+    WXS, WXA, WCT, aWCT, coi, freq, sig = wct_modified(cur, ref, dt, dj, s0, J=J, wavelet=wvn, sig=sig, siglvl=siglvl)
 end
